513f77231443a6745bed12d805f456f854e102cf
dreamseekerfromn <sy@Sungs-Air.fios-router.home>
2023-11-05 15:08:04 -0500

    adds schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS thread_db_dev;
+CREATE DATABASE thread_db_dev;
+
+\c thread_db_dev;
+
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    user_name TEXT NOT NULL,
+    user_password TEXT NOT NULL,
+    manager BOOLEAN
+);
+
+CREATE TABLE forums (
+    thread_id SERIAL PRIMARY KEY,
+    user_name TEXT REFERENCES users (user_name) ON DELETE CASCADE,
+    time_stamp TEXT NOT NULL,
+    thread_message TEXT NOT NULL,
+    profile_pic TEXT NOT NULL,
+    message_pic TEXT NOT NULL,
+);
