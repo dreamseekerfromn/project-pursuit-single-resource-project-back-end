5715357ae9c1ff9fd68f5326667bed2f516311f5
dreamseekerfromn <sy@Sungs-Air.fios-router.home>
2023-11-05 15:25:03 -0500

    adds basic seeds for db

---
+++ db/seed.sql
@@ -0,0 +1,11 @@
+\c thread_db_dev;
+
+INSERT INTO users (user_name, user_password, manager)
+VALUES
+('rolling_stones', 'abc1234', false),
+('david_bowie', 'asdf1234', false),
+('manager', 'password', true);
+
+INSERT INTO forums (user_name, time_stamp, thread_message, profile_pic, message_pic)
+VALUES 
+('manager', '1699215839', 'Hello World!', '', '');
