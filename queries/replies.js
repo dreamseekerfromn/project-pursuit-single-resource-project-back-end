b205ab2c79ca7f0cd0c5af724f2bba28a15f4766
dreamseekerfromn <sy@Sungs-Air.fios-router.home>
2023-11-13 19:28:19 -0500

    still working on reply

--- queries/replies.js
+++ queries/replies.js
@@ -11,10 +11,10 @@ const getReplies = async (id) => {
 
 const createReply = async (item) => {
     console.log(item)
-    const { reply_id, thread_id, reply_user, reply_message, reply_pw } = item;
+    const { time_stamp, thread_id, reply_user, reply_message, reply_pw } = item;
     try {
-        const message = await db.one(`INSERT INTO replies (thread_id, reply_user, reply_message) 
-            VALUES ($1, $2, $3) RETURNING *`, [thread_id, reply_user, reply_message]);
+        const message = await db.one(`INSERT INTO replies (thread_id, reply_user, reply_message, reply_timestamp) 
+            VALUES ($1, $2, $3, $4) RETURNING *`, [thread_id, reply_user, reply_message, time_stamp]);
         const reply_pw_table = await db.one(`INSERT INTO reply_pw (reply_id, reply_pw) VALUES ($1, $2)`, [message.reply_id, reply_pw])
         return message;
     } catch(err){
