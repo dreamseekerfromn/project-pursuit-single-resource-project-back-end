5ef92ad3f84976ebea17015d3f1d0d7a9ea58ebb
dreamseekerfromn <sy@Sungs-Air.fios-router.home>
2023-11-15 17:52:26 -0500

    fix errors

--- queries/replies.js
+++ queries/replies.js
@@ -10,15 +10,13 @@ const getReplies = async (id) => {
 }
 
 const createReply = async (item) => {
-    console.log(item)
     const { reply_timestamp, thread_id, reply_user, reply_message, reply_pw } = item;
     
     try {
         const message = await db.one(`INSERT INTO replies (thread_id, reply_user, reply_message, reply_timestamp) 
             VALUES ($1, $2, $3, $4) RETURNING *`, 
             [thread_id, reply_user, reply_message, reply_timestamp]);
-        console.log(`message is...`);
-        console.log(message);
+
         const reply_pw_table = await db.one(`INSERT INTO reply_pw (reply_id, reply_pw) VALUES ($1, $2)`, [message.reply_id, reply_pw])
         return message;
     } catch(err){
@@ -27,9 +25,10 @@ const createReply = async (item) => {
 }
 
 const updateReply = async(id, body) => {
-    const { reply_timestamp, reply_message, reply_pw } = item;
+    const { reply_timestamp, reply_message, reply_pw } = body;
     try {
-        const pw = await db.one(`SELECT * FROM reply_pw WHERE reply_id = ${reply_id}`);
+        const pw = await db.one(`SELECT * FROM reply_pw WHERE reply_id = ${id}`);
+
         if (pw.reply_pw == reply_pw){
             try{
                 const message = await db.any(`UPDATE replies SET reply_timestamp=$1, reply_message=$2 WHERE reply_id = ${id} RETURNING *`,[reply_timestamp, reply_message]);
@@ -41,24 +40,25 @@ const updateReply = async(id, body) => {
             return {error: "wrong pw"};
         }
     } catch(err){
+        console.log(err);
         return err;
     }
 }
 
 
 const deleteOneReply = async(id, body) => {
-    console.log(body);
     const {reply_pw} = body;
-    console.log("reply_pw is " + reply_pw);
+
     try {
         const pw = await db.one(`SELECT * FROM reply_pw WHERE reply_id = ${id}`);
-        console.log("pw is " + pw);
+
         if (pw.reply_pw == reply_pw){
             try{
                 console.log("entered")
                 const message = await db.one(`DELETE FROM replies WHERE reply_id = ${id} RETURNING *`);
                 return message;
             } catch(err){
+                console.log(err)
                 return err;
             }
         }
